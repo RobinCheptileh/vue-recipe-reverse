f1a40ddd7223a9531a1859c78f5cb401993ba222
Robin Cheptileh <robin@sendyit.com>
2019-10-14 01:00:46 +0300

    :spakles: :art: :lipstick: :zap: add api search

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,61 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <div id="nav">
+            <b-navbar variant="faded" type="light">
+                <b-navbar-brand to="/">
+                    <img id="logo" alt="Recipe Reverse" src="./assets/logo.png">
+                    Recipe Reverse
+                </b-navbar-brand>
+
+                <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+                <b-collapse id="nav-collapse" is-nav>
+                    <b-navbar-nav class="ml-auto">
+                        <b-nav-item to="/about">About</b-nav-item>
+                    </b-navbar-nav>
+                </b-collapse>
+            </b-navbar>
+        </div>
+        <router-view/>
+        <footer class="align-content-center">
+            <p>
+                Made with
+                <unicon name="heartbeat" fill="red"></unicon>
+            </p>
+        </footer>
     </div>
-    <router-view/>
-  </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
     }
-  }
-}
+
+    #nav {
+        padding: 30px;
+
+        a {
+            font-weight: bold;
+            color: #2c3e50;
+
+            &.router-link-exact-active {
+                color: #008577;
+            }
+        }
+
+        img {
+            vertical-align: middle;
+
+            &#logo {
+                height: 50px;
+            }
+        }
+    }
+
+    footer {
+        padding: 50px;
+    }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
-import '@babel/polyfill'
-import 'mutationobserver-shim'
+import '@babel/polyfill';
+import 'mutationobserver-shim';
 import Vue from 'vue';
-import './plugins/bootstrap-vue'
+
+// Plugins
+import './plugins/bootstrap-vue';
+import './plugins/vue-unicons';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -10,7 +13,7 @@ import './registerServiceWorker';
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
+    router,
+    store,
+    render: h => h(App),
 }).$mount('#app');
