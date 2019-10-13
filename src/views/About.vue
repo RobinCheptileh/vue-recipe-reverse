0a572e424f5e8949a21174375f99a12084d00f22
Robin Cheptileh <robin@sendyit.com>
2019-10-14 01:00:46 +0300

    :sparkles: :art: :lipstick: :zap: add api search

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <img id="logo" alt="Recipe Reverse" src="../assets/logo.png" class="align-self-center">
+        <h1>About</h1>
+        <p>Recipe Reverse allows you to find a popular recipe by specifying the ingredients you have.</p>
+    </div>
 </template>
+
+<style scoped lang="scss">
+    img {
+        &#logo {
+            height: 200px;
+            margin-bottom: 50px;
+        }
+    }
+</style>
