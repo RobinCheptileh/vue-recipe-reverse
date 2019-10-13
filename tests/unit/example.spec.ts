0ce792f08e3d00446b1b6db75608e6545d52c18c
Robin Cheptileh <robin@sendyit.com>
2019-10-14 01:00:46 +0300

    :spakles: :art: :lipstick: :zap: add api search

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,13 +1,13 @@
 import { expect } from 'chai';
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import Recipe from '@/components/Recipe.vue';
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg },
+describe('Recipe.vue', () => {
+  it('renders props.name when passed', () => {
+    const name = 'Mac and Cheese';
+    const wrapper = shallowMount(Recipe, {
+      propsData: { name },
     });
-    expect(wrapper.text()).to.include(msg);
+    expect(wrapper.text()).to.include(name);
   });
 });
