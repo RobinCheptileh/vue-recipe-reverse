9e8c010393550ba17d346b0b88b8ac615ee3d074
Robin Cheptileh <robin@sendyit.com>
2019-10-14 12:00:03 +0300

    :heavy_plus_sign: lodash and axios :zap: debounce search requests

--- src/services/RecipeService.ts
+++ src/services/RecipeService.ts
@@ -1,15 +1,16 @@
 import {Recipe} from '@/models/Recipe';
+import axios from 'axios';
 
 export default class RecipeService {
     public static readonly BASE_URL = 'https://recipe-reverse-api.herokuapp.com/recipes';
 
     public static getRecipes(): Promise<Recipe[]> {
-        return fetch(RecipeService.BASE_URL)
-            .then(data => data.json());
+        return axios.get(RecipeService.BASE_URL)
+            .then(response => response.data);
     }
 
     public static searchRecipe(ingredients: string): Promise<Recipe[]> {
-        return fetch(`${RecipeService.BASE_URL}/recommendations?ingredients=${ingredients}`)
-            .then(data => data.json());
+        return axios.get(`${RecipeService.BASE_URL}/recommendations?ingredients=${ingredients}`)
+            .then(response => response.data);
     }
 }
