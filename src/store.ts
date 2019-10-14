6668e9dea7a7552b92a04999589465fe5e030447
Robin Cheptileh <robin@sendyit.com>
2019-10-14 11:18:47 +0300

    :art: add recipe service :zap: integrate vuex

--- src/store.ts
+++ src/store.ts
@@ -1,16 +1,32 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import {Recipe} from '@/models/Recipe';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  },
+    state: {
+        ingredients: '',
+        recipes: [] as Recipe[],
+    },
+    mutations: {
+        setIngredients(state, ingredients: string) {
+            return state.ingredients = ingredients;
+        },
+        setRecipes(state, recipes: Recipe[]) {
+            return state.recipes = recipes;
+        },
+    },
+    getters: {
+        ingredients: state => state.ingredients,
+        recipes: state => state.recipes,
+    },
+    actions: {
+        setIngredients(context, ingredients: string) {
+            context.commit('setIngredients', ingredients);
+        },
+        setRecipes(context, recipes: Recipe[]) {
+            context.commit('setRecipes', recipes);
+        },
+    },
 });
